a0679d3fc47fe35dc83e5f52ecca666c90455f10
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-07 22:58:25 +0600

    Fix: Correct syntax errors and structure in User.cs and rename solution file

--- Entities/user.cs
+++ Entities/user.cs
@@ -1,49 +1,38 @@
-namespace TwitterClone.Domain.Entities{
-    
-    
-    public class User {
-        
-        
-        private Guid _id ;
-        private string _firstname ;
-        private string _lastname ;
-        private strting _email; 
-        
-        
-        public User ()
+namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public User()
         {
-            _id =Guid.NewGuid()
+            _id = Guid.NewGuid();
         }
-        public Guid Id 
+
+        public Guid Id
         {
-            get 
-            {
-                return _id ;
-                
-            }
+            get { return _id; }
         }
-        
-        public string FirstName 
+
+        public string FirstName
         {
-            get 
-            {
-                return _firstName;
-                
-            }
-            set 
-            {
-                _firstName = value ;
-            }
-            
+            get { return _firstName; }
+            set { _firstName = value; }
         }
-        
-        
-        
-        
-        
-        
-        
-        
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
     }
-    
 }
