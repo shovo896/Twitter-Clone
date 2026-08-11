171d4f28d374250893c3705caf661019f73cb8b9
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-11 22:44:52 +0600

    hjnbk

---
+++ Entities/Like.cs
@@ -0,0 +1,30 @@
+namespace Twitter_Clone.Entities;
+
+public class Like
+{
+    private Guid _id; 
+    private Guid _userId;
+    private Guid _tweetId; 
+    private DateTime _createdAt;
+    private DateTime _modifiedAt;
+    private Guid _createBy; 
+    private Guid _modifiedBy;
+
+    public Like()
+    {
+        _id = Guid.NewGuid(); 
+        _createdAt = DateTime.UtcNow;
+    }
+
+    public Guid Id
+    {
+        get { return _id; }
+    }
+
+    public Guid UserId
+    {
+        get { return _userId; }
+        set {  _userId = value; }
+        
+    }
+}
