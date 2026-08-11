882e40d4155d8f7fc7a95af7f37608bb80d53c01
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-12 01:45:54 +0600

    jbnjkkjkn

--- Entities/Bookmark.cs
+++ Entities/Bookmark.cs
@@ -2,5 +2,25 @@ namespace Twitter_Clone.Entities;
 
 public class Bookmark
 {
-    
+    private Guid _id;
+    private Guid _userId;
+    private Guid _tweetId;
+    private DateTime _createdAt;
+    private DateTime _modifiedAt;
+    private Guid _createdBy;
+    private Guid _modifiedBy;
+
+    public Bookmark()
+    {
+        _id = Guid.NewGuid();
+        _createdAt = DateTime.UtcNow;
+    }
+
+    public Guid Id => _id;
+    public Guid UserId => _userId;
+    public Guid TweetId => _tweetId;
+    public DateTime CreatedAt => _createdAt;
+    public DateTime ModifiedAt => _modifiedAt;
+    public Guid CreatedBy => _createdBy;
+    public Guid ModifiedBy => _modifiedBy;
 }
