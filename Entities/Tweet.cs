fff52990ca2b7e0971ba2acabfcd75b2c3df63ce
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-12 14:15:29 +0600

    dhu

--- Entities/Tweet.cs
+++ Entities/Tweet.cs
@@ -1,25 +1,28 @@
 using System.Text ;
 using System.Threading.Tasks;
+using Twitter_Clone.Entities;
 
 namespace TwitterClone.Domain.Entities
 {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public class Tweet
+    public class Tweet :BaseEntity
     {
+        // private Guid _id;
+        private Guid _userId;
+        private string _content; 
+            //private DateTime _createdAt; 
+       // private DateTime _modifiedAt;
+        //private Guid _createdBy; 
+        // private Guid _modifiedBy;
+
+
+        public Tweet(string content):base(Guid.NewGuid())
+        {
+            //_id = Guid.NewGuid(); 
+             // _createdAt = DateTime.UtcNow;
+             _content = content; 
+             
+        }
         public string Content { get; private set; }
 
         public void AddContent(string content)
