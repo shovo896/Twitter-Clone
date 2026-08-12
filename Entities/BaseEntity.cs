480fddbda74adb61de3eeb4b57740b23e45558e9
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-12 13:23:18 +0600

    hjnbjkl

--- Entities/BaseEntity.cs
+++ Entities/BaseEntity.cs
@@ -2,7 +2,7 @@
  using  System.Collections.Generic; 
  using System.Linq; 
  using System.Text; 
- using System.Threading.Tasks;
+ using System.Threading.Tasks
 
 
 
@@ -19,5 +19,27 @@
    private DateTime _modifiedAt;
    private Guid _createdBy; 
    private Guid _modifiedBy; 
+   
+   
+   
+   public Guid Id { get; private  set; }
+
+   public DateTime CreatedAt { get; private set; }
+
+    
+   public DateTime? ModifiedAt { get; private set; }
+    
+    public Guid CreatedBy { get; private set; } 
+    public Guid? ModifiedBy { get; private set; } 
+    public BaseEntity()
+    {
+      //_createdAt = DateTime.Now;
+     //  _modifiedAt = DateTime.Now;
+     Id = Guid.NewGuid(); 
+     CreatedAt = DateTime.UtcNow; 
+     
+    }
+   
+
   }
  }
