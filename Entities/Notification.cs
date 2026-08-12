f1aad4410a975e9dffd521e122f7d75a97b7643f
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-12 14:18:54 +0600

    khjbkj

---
+++ Entities/Notification.cs
@@ -0,0 +1,20 @@
+namespace Twitter_Clone.Entities;
+
+public class Notification :BaseEntity
+{
+    private Guid _userId;
+    private string _type;
+    private string _message;
+    private bool isRead;
+
+    public Notification()
+    {
+        
+    }
+
+    public Guid UserId
+    {
+        get { return _userId; } 
+        set { _userId = value; }
+    }
+}
